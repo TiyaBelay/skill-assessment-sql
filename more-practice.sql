c0bf7b83c9254db77c7aad40f88a7665c62c28ff
Tiya Belay <TMD@Timothys-MacBook-Pro.local>
2016-04-30 19:58:35 -0700

    Completed more-practice assessment

--- more-practice.sql
+++ more-practice.sql
@@ -3,9 +3,10 @@
 
 
 -- INSERT
-
+INSERT INTO models (year, brand_name, name) VALUES (2015, 'Chevrolet', 'Malibu'), (2015, 'Subaru', 'Outback');
 
 -- CREATE TABLE
-
+CREATE TABLE awards(name VARCHAR(100) NOT NULL, year INTEGER, winner_id SERIAL PRIMARY KEY);
 
 -- More INSERT
+INSERT INTO awards (name, year, winner_id) VALUES ('IIHS Safety Award Year:', 2015, 49), ('IIHS Safety Award Year:', 2015, 50);
